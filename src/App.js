69d9fcd2cff308be329834486d624cb9484290b0
Cypher890 <abdulsuhaib07@gmail.com>
2022-12-13 17:53:21 +0530

    added overlay to the food cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
@@ -7,6 +8,7 @@ import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
