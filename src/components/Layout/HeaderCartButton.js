b32651a147f454b019b6d27b934c8332817f9c0d
Cypher890 <abdulsuhaib07@gmail.com>
2022-12-15 13:07:53 +0530

    completed the food kart by adding the removing and add functionalities to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+ const[btnHighlite,setBtnHighlited]= useState(false)
   const cartCtx = useContext(CartContext);
-  const numOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const {items} = cartCtx
+  const numOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount
   },0)
+
+  const btnClasses = `${classes.button} ${btnHighlite ? classes.bump: ''}`
+
+useEffect(()=>{
+  if(items.length === 0){
+    return
+  }
+  setBtnHighlited(true)
+
+ const timer = setTimeout(()=>{
+    setBtnHighlited(false)
+  }, 300)
+  return ()=>{
+    clearTimeout(timer)
+  }
+},[items])
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
