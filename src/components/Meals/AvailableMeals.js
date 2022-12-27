b189b8683800cf8b4f7123302a720fd14ada36e3
Cypher890 <abdulsuhaib07@gmail.com>
2022-12-27 15:36:07 +0530

    including http functon

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./Mealitem/MealItem";
 
-const DUMMY = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 function AvailableMeals() {
-  const mealsList = DUMMY.map((meal) => (
+  const [meals, setMeals] = useState([])
+
+  useEffect(()=> {
+    const fetchMeals = async()=>{
+     const response = await fetch('https://reactfoodcart-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json')
+     const responseData = await response.json()
+
+     const loadedMeals = []
+    //  console.log(responseData)
+     
+      for(const key in responseData) {
+      //  console.log(`${key} ${responseData[key].name} ${responseData.m1.name}`)
+        loadedMeals.push({
+          id:key,
+          name:responseData[key].name,
+          description: responseData[key].description,
+          price:responseData[key].price
+        })
+      }
+      setMeals(loadedMeals)
+    }
+    fetchMeals()
+  },[])
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
